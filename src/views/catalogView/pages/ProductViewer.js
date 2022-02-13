4b357e8c20c15436e5199b223e9a5d9e797180ba
Acetylcholine007 <66874501+Acetylcholine007@users.noreply.github.com>
2022-02-13 09:57:04 +0800

    ProductViewer UI fiexed

--- src/views/catalogView/pages/ProductViewer.js
+++ src/views/catalogView/pages/ProductViewer.js
@@ -1,10 +1,9 @@
-import { Grid } from "@mui/material";
+import { Grid, Divider } from "@mui/material";
 import React, { useContext } from "react";
 import { useHistory, useParams } from "react-router-dom";
 import { Star, StarBorder } from "@mui/icons-material";
 import { InventoryContext } from "../../../shared/contexts/InventoryContextProvider";
 import { useUserContext } from "../../../shared/hooks/useUserContext";
-import CommentStub from "../components/CommentStub";
 import "./ProductViewer.css";
 
 const ProductViewer = () => {
@@ -18,12 +17,12 @@ const ProductViewer = () => {
 
   const ratingList = [];
   if (product.ratings != null)
-    for (var rating in product.ratings) ratingList.push(product.ratings[rating]);
+    for (var rating in product.ratings)
+      ratingList.push(product.ratings[rating]);
   const ratings =
     ratingList.length !== 0
       ? ratingList.reduce((a, b) => a + b) / ratingList.length
       : 0;
-
 
   const cartHandler = (pid) => {
     userDispatch({
@@ -50,7 +49,7 @@ const ProductViewer = () => {
     return (
       <div className="product-viewer">
         <Grid container spacing={4}>
-          <Grid item xs={12} md={6}>
+          <Grid item xs={12} md={5}>
             <div className="viewer-image-container">
               <img
                 src={product.uri}
@@ -59,45 +58,36 @@ const ProductViewer = () => {
               />
             </div>
           </Grid>
-          <Grid item xs={12} md={6}>
-            <h1>{product.name}</h1>
-            <h3 style = {{verticalAlign: 'center'}}>
+          <Grid item xs={12} md={7}>
+            <h1 className="product-name">{product.name}</h1>
+            <h3 style={{ verticalAlign: "center" }}>
               <span>
                 {[1, 2, 3, 4, 5].map((count) => {
                   if (count > ratings) return <StarBorder key={count} />;
                   else return <Star key={count} />;
                 })}
               </span>
-              <span>HOT</span>
             </h3>
-            <h1>Price: {product.price}</h1>
-            <button
-              className="app-button"
-              onClick={() => cartHandler(product.productId)}
-            >
-              ADD TO CART
-            </button>
-            <button
-              className="app-button red"
-              onClick={() => buyHandler(product)}
-            >
-              BUY NOW
-            </button>
-            <h3>Description:</h3>
+            <div className="price-bar">
+              <h1 className="currency">&#8369;</h1>
+              <h1>
+                {product.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+              </h1>
+            </div>
+            <div className="product-actions">
+              <button onClick={() => cartHandler(product.productId)}>
+                ADD TO CART
+              </button>
+              <button onClick={() => buyHandler(product)}>BUY NOW</button>
+            </div>
+            <Divider />
+            <h2>Description:</h2>
             <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
               officiis doloremque enim magni necessitatibus suscipit alias atque
               voluptate illo quas hic eius adipisci nemo iste, perferendis
               dolore ullam veniam voluptas.
             </p>
-          </Grid>
-          <Grid item xs={12}>
-            <h3>Comments:</h3>
-            <ul className="comment-list">
-              {product.comment.map((comment, index) => (
-                <CommentStub comment={comment} key={index} />
-              ))}
-            </ul>
           </Grid>
         </Grid>
       </div>
